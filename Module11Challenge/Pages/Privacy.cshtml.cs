6179f18fb36ce99b601148e3e3fab1c5dabc0be5
Ryad Abdelgalil <Ryad.Abdelgalil@go.minneapolis.edu>
2026-04-07 16:42:49 +0000

    Initial Commit

---
+++ Module11Challenge/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Module11Challenge.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
